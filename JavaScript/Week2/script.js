b13af2c85a097c4c546c01165a0ef3d829f18c85
Noman Atique <noman.atique@keyin.com>
2024-05-07 12:33:59 -0400

    may7

---
+++ JavaScript/Week2/script.js
@@ -0,0 +1,51 @@
+// var x;
+// x = 20;
+// console.log(x);
+
+// x = "Alan";
+// console.log(x);
+
+// var x = 10;
+// console.log(x++); // 10  11
+// console.log(++x); //12
+// console.log(--x); //11
+// console.log(x--); //11
+
+// var x = 0;
+// var b = [];
+
+// b[x++] = 10; //b[0]? b[1]
+// b[++x] = 20;
+// console.log(b);
+
+// let x = "10";
+// if (x === 10) {
+//   console.log("YES");
+// } else {
+//   console.log("NO");
+// }
+
+// Always try to use === in place of == to save yourself coercion!
+
+// let age = 10;
+// // if (age > 18) {
+// //   console.log("Welcome");
+// // } else {
+// //   console.log("Underage");
+// // }
+
+// // age > 18 ? console.log("Welcome") : console.log("Underage");
+
+// let message = age > 18 ? "Welcome" : "Underage";
+
+// typeof
+
+// var x = 10;
+// console.log(typeof x);
+
+// x = "Alan";
+// console.log(typeof x);
+
+for (var i = 1; i <= 10; ++i) {
+  console.log(i);
+}
