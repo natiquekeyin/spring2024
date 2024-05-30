ddffce7396d818ceb9303e95f2985750fc32f1da
Noman Atique <noman.atique@keyin.com>
2024-05-30 11:13:59 -0400

    may30_library

--- Front-end/Week5/scripts/library.js
+++ Front-end/Week5/scripts/library.js
@@ -2,9 +2,11 @@ window.addEventListener("DOMContentLoaded", function () {
   let form = document.querySelector("#form1");
 
   form.addEventListener("submit", function (e) {
+    // go and collect user input from three input boxes and store them in js variables here
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let isbn = document.querySelector("#isbn").value;
+
     addBookToList(title, author, isbn); //addBookToList() will create a new row in the table and insert title author and isbn there in the row
 
     e.preventDefault();
@@ -15,8 +17,8 @@ window.addEventListener("DOMContentLoaded", function () {
       showAlert("No field should be empty", "error");
     } else {
       let row = document.createElement("tr");
-      row.innerHTML = `<td>${t}</td><td>${a}</td><td>${i}</td><td><button>X</button></td>`;
-
+      row.innerHTML = `<td>${t}</td><td>${a}</td><td>${i}</td><td><button class="delete">X</button></td>`;
+      // append the newly created row in the table body with id book-list
       document.querySelector("#book-list").appendChild(row);
       clearFields();
       showAlert("Book successfully added!", "success");
@@ -40,4 +42,23 @@ window.addEventListener("DOMContentLoaded", function () {
       document.querySelector("#box").remove();
     }, 3000);
   }
+
+  this.document.querySelector("#data").addEventListener("click", function (e) {
+    deleteBook(e.target);
+  });
+
+  function deleteBook(elemToDelete) {
+    if (elemToDelete.className === "delete") {
+      elemToDelete.parentElement.parentElement.remove();
+      showAlert("Book removed successefully", "success");
+    } else {
+      showAlert("Wrong area clicked, please click on X", "error");
+    }
+  }
 });
+
+// A todo application????
+
+// Convert it into real object oriented programming by introducting the concept of Classes and objects...
+
+// Use localStorage to make books permanent, even after the refresh of the page the books will appear again as they will be residing in our browser localStorage..
