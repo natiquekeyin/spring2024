b51294fd4ffd24dbbdc68f965e74ef24b807f9d6
Noman Atique <noman.atique@keyin.com>
2024-06-12 08:35:46 -0400

    june12

--- JavaScript/Week6&7/scripts/xhr.js
+++ JavaScript/Week6&7/scripts/xhr.js
@@ -15,9 +15,77 @@ function loadData() {
       document.querySelector(
         "#output"
       ).innerHTML = ` <strong> ${this.responseText}</strong>`;
+    } else {
+      console.log("Something went wrong");
     }
   };
 
   //   send the request..
+  xhr.send();
+}
+
+document.querySelector("#btn2").addEventListener("click", loadJSON);
+
+function loadJSON() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "./data/user.json");
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      // console.log(this.responseText);
+      let user = JSON.parse(this.responseText);
+      let output = `<ul><li>ID: ${user.id}</li><li>NAME: ${user.name}</li><li>EMAIL:${user.email}</li><li>PHONE: ${user.phone}</li></ul>`;
+
+      document.querySelector("#output").innerHTML = output;
+    }
+  };
+
+  xhr.send();
+}
+
+document.querySelector("#btn3").addEventListener("click", loadJSONS);
+
+function loadJSONS() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "./data/users.json");
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      // console.log(this.responseText);
+      let users = JSON.parse(this.responseText);
+
+      let output = "";
+      users.forEach(function (user) {
+        output += `<div class="profile"><h3>ID: ${user.id}</h3><p>NAME: ${user.name}</p><p>EMAIL:${user.email}</p><p>PHONE: ${user.phone}</p></div>`;
+      });
+
+      // let output = `<ul><li>ID: ${user.id}</li><li>NAME: ${user.name}</li><li>EMAIL:${user.email}</li><li>PHONE: ${user.phone}</li></ul>`;
+
+      document.querySelector("#output").innerHTML = output;
+    }
+  };
+
+  xhr.send();
+}
+
+document.querySelector("#btn4").addEventListener("click", loadAPI);
+
+function loadAPI() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://reqres.in/api/users");
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let users = JSON.parse(this.responseText);
+      let output = "";
+      users.data.forEach(function (user) {
+        output += `<div class="profile"><p><img src = "${user.avatar}" id="pic"/></p><h1>${user.id}</h1><p>${user.first_name}</p></div>`;
+      });
+      document.querySelector("#output").innerHTML = output;
+    }
+  };
   xhr.send();
 }
