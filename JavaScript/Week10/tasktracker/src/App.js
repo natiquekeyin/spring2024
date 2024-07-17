552940675e49fc6504014cef030e1be546c8f2e0
Noman Atique <noman.atique@keyin.com>
2024-07-17 08:31:57 -0400

    july17

--- JavaScript/Week10/tasktracker/src/App.js
+++ JavaScript/Week10/tasktracker/src/App.js
@@ -11,15 +11,28 @@ function App() {
 
   // to delete a task
 
-  const deleteTask = (id) => {
-    // console.log("delete", id);
+  const deleteTask = async (id) => {
+    // bring the task with given id..
+
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
 
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // to toggleReminder true or false
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
@@ -29,16 +42,34 @@ function App() {
 
   // to add a task...
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newTask = { id, ...task }; //create a new task and embed the id with it
-    setTasks([...tasks, newTask]); //take the tasks and embed new task in that array
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 1000) + 1;
+    // const newTask = { id, ...task }; //create a new task and embed the id with it
+    // setTasks([...tasks, newTask]); //take the tasks and embed new task in that array
   };
 
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch("http://localhost:5000/tasks"); //get request API
     const data = await res.json();
 
+    return data;
+  };
+
+  // fetchTask ...singular
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
     return data;
   };
 
